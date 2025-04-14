cc434f3acae879f0f8188a8391fde5acf9c4171f
ANUS AHMED <w3anasahmed@gmail.com>
2025-04-14 10:42:46 +0500

    Update calculator.dart

--- calculator.dart
+++ calculator.dart
@@ -1,1 +1,15 @@
+import "dart:io";
 
+void main() {
+ print("""Hello! It's a Calculator Program.
+1. Percentage & Grade Finder.
+2. Even Number Printer & Counter.  
+3. Factorial Calculator.  
+4. Leap Year Finder (From [x] to [y]).  
+5. Arithmetic Operations Calculator.  
+6. Length Unit Converter.
+ """);
+  print("Enter the S.No to Perform the calculation");
+  int? s_No = int.parse(stdin.readLineSync()!);
+  
+}
